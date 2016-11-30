aa665e7c98ef9a1715bb546d0862546b2a155c5e
WalTesco <lanceuppercut.lu@gmail.com>
2016-11-29 22:15:47 -0600

    Add files via upload

--- MX25R512F.py
+++ MX25R512F.py
@@ -9,16 +9,20 @@ spi.pins = PIN_POWER | PIN_CS
 spi.config = CFG_PUSH_PULL | CFG_CLK_EDGE
 spi.speed = '30kHz'
 
-def Read(address, rx_bytes): # TESTED GOOD
+# Read Byte. Address Passed is MSB First
+
+def Read(address, rx_bytes):
     data = bytes()
     spi.cs = True
-    spi.transfer([0x03, address[0], address[1], address[2]]) # Read Byte. Address Passed is MSB First
+    spi.transfer([0x03, address[0], address[1], address[2]]) 
     for i in range(rx_bytes) :
-        data += spi.transfer([0xFF]) #NOTE!!!! With MX25R512 Only the last 2 address bytes are used
+        data += spi.transfer([0xFF])
     spi.cs = False 
     return data
 
-def Fast_Read(address, rx_bytes): # TESTED GOOD
+# Fast Read.
+
+def Fast_Read(address, rx_bytes):
     data = bytes()
     spi.cs = True
     spi.transfer([0x0B, address[0], address[1], address[2], 0xFF])
@@ -27,115 +31,143 @@ def Fast_Read(address, rx_bytes): # TESTED GOOD
     spi.cs = False
     return data
 
-def X2READ(): # Double Transfer Rate Read
+# Double Transfer Rate Read. (Unsupported)
+
+def X2READ(): 
     pass
 
-def DREAD(): # Dual Rate Read
+# Dual Rate Read. (Unsupported)
+
+def DREAD(): 
     pass
 
-def X4READ(): # Quad Transfer Rate Read (Not Supported)
+# Quad Transfer Rate Read. (Unsupported)
+
+def X4READ(): 
     pass
 
-def QREAD(): # Quad Rate Read (Not Supported)
+# Quad Rate Read. (Unsupported)
+
+def QREAD(): 
     pass
 
-def PP(Address, data): #TESTED WORKING
+# Page Program/Write.
+
+def PP(Address, data):
     spi.cs = True
-    spi.transfer([0x02, Address[0], Address[1], Address[2]]) # Page Program
+    spi.transfer([0x02, Address[0], Address[1], Address[2]])
     for i in range(0,len(data)):
         spi.transfer([data[i]])
     spi.cs = False
 
-def X4PP(): # Not Supported
+# Quad Transfer Page Program. (Unsupported)
+
+def X4PP():
     pass
 
+# Sector Erase
 
-def Sector_Erase(Address): #TESTED WORKING
+def Sector_Erase(Address): 
     spi.cs = True
-    spi.transfer([0x20, Address[0] , Address[1], Address[2]]) # Erase Sector
+    spi.transfer([0x20, Address[0] , Address[1], Address[2]]) 
     spi.cs = False
-    
-def Block_Erase_32(Address): #TESTED WORKING
+
+# 32-Byte Block Erase
+
+def Block_Erase_32(Address):
     spi.cs = True
     spi.transfer([0x52, Address[0], Address[1], Address[2]])
     spi.cs = False
 
-def Block_Erase_64(Address): #TESTED WORKING
+# 64-Byte Block Erase
+
+def Block_Erase_64(Address):
     spi.cs = True
     spi.transfer([0xD8, Address[0], Address[1], Address[2]])
     spi.cs = False
-    
-def Chip_Erase(): #TESTED WORKING
+
+# Chip Erase
+
+def Chip_Erase():
     spi.cs = True
-    spi.transfer([0x60]) # Erase Entire Chip. BP0-4 Must Be Disabled, Raise Error If Not Disabled
+    spi.transfer([0x60])
     spi.cs = False
 
-def RDSFDP(Address,RX_Bytes): #TESTED WORKING
+# Read JEDEC SFDP Data
+
+def RDSFDP(Address,RX_Bytes):
     data = bytes()
     spi.cs = True
     spi.transfer([0x5a, Address[0], Address[1], Address[2], 0xff])
     for i in range(0, RX_Bytes):
-        data += spi.transfer([0xff]) # Read SFDP Data
+        data += spi.transfer([0xff])
     spi.cs = False
     return data
 
-def WREN(): #TESTED GOOD
+# Set Write Enable Latch
+
+def WREN():
     spi.cs = True
-    spi.transfer([0x06]) # Set Write Enable Latch
+    spi.transfer([0x06])
     spi.cs = False
 
-def WRDI(): #TESTED GOOD
+# Disable Writing
+
+def WRDI():
     spi.cs = True
-    spi.transfer([0x04]) # Set Write Disable
+    spi.transfer([0x04])
     spi.cs = False
+
+# Read Status Register
     
-def RDSR(): #TESTED GOOD
+def RDSR():
     #data = bytes()
     spi.cs = True
-    data = spi.transfer([0x05, 0xFF]) # Read Status Register
-    spi.cs = False #Status Register Can Be Checked At Any Time During Any Operation
-    return data[1:]
-
-def RDCR(): #TESTED GOOD
-    data = bytes()
-    spi.cs = True
-    data += spi.transfer([0x15, 0xFF,0xFF]) # Read Configuration Register
+    data = spi.transfer([0x05, 0xFF])
     spi.cs = False
     return data[1:]
 
-#This function is not finished
+# Read Configuration Register
 
-def WRSR(SR_IN): #Tested GOOD
+def RDCR():
+    data = bytes()
     spi.cs = True
-    spi.transfer([0x01, SR_IN[0], SR_IN[1], SR_IN[2]]) # Write Status Register
-    spi.cs = False # Remember To Set Write Enable Latch(WREN) Before Sending Command
+    data += spi.transfer([0x15, 0xFF,0xFF]) 
+    spi.cs = False
+    return data[1:]
 
+# Write Status Register
 
-def Suspend(): # Suspend Program/Erase
+def WRSR(SR_IN):
+    spi.cs = True
+    spi.transfer([0x01, SR_IN[0], SR_IN[1], SR_IN[2]]) 
+    spi.cs = False
+
+# Suspend Program/Erase
+
+def Suspend():
     spi.cs = True
     spi.transfer([0x75])
     spi.cs = False
 
-def Resume(): # Resumes Program/Erase
+# Resumes Program/Erase
+
+def Resume(): 
     spi.cs = True
     spi.transfer([0x7A])
     spi.cs = False
+    
+# Deep Power Down Mode
 
-def DP(): # Deep Power Down Mode
-    pass #Can not support this feature with BP alone
+def DP(): 
+    pass
     
+# Deep Power Down Mode Release. (Unsupported)
     
-    
-def DP_Release(): # Unsupported
+def DP_Release(): 
     pass
 
-# Make sure all this shit is right
-
-Burst_Depth = {
-    'X8' : 0x00,
-    'X16' : 0x01,
-    'X32' : 0x02,
-    'X64' : 0x03}
+# Enable Burst Read And Set Depth
     
 def Burst_Read(BurstDepth, Wrap):
     if Wrap == True :
@@ -152,99 +184,129 @@ def Burst_Read(BurstDepth, Wrap):
         Wrap = False
         return Wrap
 
-def RDID(): #TESTED GOOD
+# Read Device ID
+
+def RDID():
     data = bytes()
     spi.cs = True
-    data += spi.transfer([0x9F, 0XFF, 0xFF, 0xFF]) # Read Device ID
+    data += spi.transfer([0x9F, 0XFF, 0xFF, 0xFF]) 
     spi.cs = False
-    return data[1:4] #data[1] = Manufacturers ID = 0xC2, data[2] = Memory Type = 0x28 , data[3] = Data Density = 0x10=16 probably bytes
+    return data[1:4] #data[1] = Manufacturers ID = 0xC2, data[2] = Memory Type = 0x28 , data[3] = Data Density = 0x10 = 16-Bytes
 
-def RES(): #TESTED GOOD
+# Read Electronic Signature
+
+def RES():
     data = bytes()
-    spi.cs = True # This Method Of Reading Electronic Signature Is Antiquated Use RDID Instead
-    data += spi.transfer([0xAB, 0xFF, 0xFF, 0xFF, 0xFF]) # Read Electronic Signature
+    spi.cs = True 
+    data += spi.transfer([0xAB, 0xFF, 0xFF, 0xFF, 0xFF]) 
     spi.cs = False
     return data[4:]
 
-def REMS(address): #TESTED GOOD
+# Read Electronic Manufacturer ID & Device ID
+
+def REMS(address):
     data = bytes()
     spi.cs = True
     spi.transfer([0x90, 0xFF, 0xFF])
-    data += spi.transfer([address, 0xFF,0xFF]) # Read Electronic Manufacturer ID & Device ID
-    spi.cs = False # If Address = 0x00 Manufacturer ID Is Read First, If Address = 0x01 Device ID Is Read First
-    return data[1:]
-    
-def ENSO(): #TESTED WORKING
-    spi.cs = True
-    spi.transfer([0xB1]) # Enter Secure One Time Programmable Mode
-    spi.cs = False
-    
+    data += spi.transfer([address, 0xFF,0xFF]) 
+    spi.cs = False 
+    return data[1:] # If Address = 0x00 Manufacturer ID Is Read First, If Address = 0x01 Device ID Is Read First
 
-def EXSO(): #TESTED WORKING
+# Enter Secure One Time Programmable Mode
+    
+def ENSO():
     spi.cs = True
-    spi.transfer([0XC1]) # Exit Secure One Time Programmable Mode
+    spi.transfer([0xB1]) 
     spi.cs = False
 
-def RDSCUR(): #TESTED WORKING
+# Exit Secure One Time Programmable Mode
+
+def EXSO():
     spi.cs = True
-    Regs = spi.transfer([0X2B, 0xFF]) # Read Security Register
+    spi.transfer([0XC1]) 
+    spi.cs = False
+
+# Read Security Register
+
+def RDSCUR():
+    spi.cs = True
+    Regs = spi.transfer([0X2B, 0xFF]) 
     spi.cs = False
     return Regs[1]
 
 #!Warning The WRSCUR() will permanently lock the secure 8k-bit onetime programmable area if used 
 
-#def WRSCUR(): # Write To Security Register
-#    spi.cs = True
-#    spi.transfer([0x2F])
-#    spi.cs = False
+def WRSCUR(): # Write To Security Register
+    spi.cs = True
+    spi.transfer([0x2F])
+    spi.cs = False
 
-def NOP(): # NO-OP Can Be Used For Timing Function 
+# NO-OP
+
+def NOP(): 
     spi.cs = True
     spi.transfer([0x00])
     spi.cs = False
 
-def RSTEN(): # Reset Enable
+# Reset Enable
+
+def RSTEN(): 
     spi.cs = True
     spi.transfer([0x66])
     spi.cs = False
+    
+# Reset Memory
 
-def RST(): # Reset Memory
+def RST(): 
     spi.cs = True
     spi.transfer([0x99])
     spi.cs = False
 
-def Release(): # Release From Read Enhance Mode
+# Release From Read Enhance Mode
+
+def Release(): 
     spi.cs = True
     spi.transfer([0xFF])
     spi.cs = False
 
-def Config_Reg(Status_Reg, Config_Reg0, Config_Reg1): # Use This With WRSR To Configure Status Register
+# Configure Status Register
+
+def Config_Reg(Status_Reg, Config_Reg0, Config_Reg1): 
     Config = [bytes()]
     Config[0] = Status_Reg
     Config[1] = Config_Reg0
     Config[2] = Config_Reg1
     return Config
+
+# Enable Enhanced Mode. (Unsupported)
     
-def Enhance_Mode(): # Skips the command cycle and saves a clock cycle for every read. Only available in High Performance Mode
+def Enhance_Mode():
     pass
+
+# Reset Enhanced Mode. (Unsupported)
+
 def Enhance_Mode_Reset():
     pass
 
-#Combine Status And Config Regs Into A Single Class Of Dicts
-
+class BURST_DEPTH():
+    X8 = 0x00
+    X16 = 0x01
+    X32 = 0x02
+    X64 = 0x03
+    
 class STATUS_REG():
     SRWD = 0b10000000 # Status Register Write Protect(0 = Status Register Write Enable, 1 = Status Register Write Disable)
     QE = 0b01000000 # Quad Enable (0 = Not Quad Enable, 1 = Quad Enable)
-    BP3 = 0b00100000 # Block Protect
-    BP2 = 0b00010000 # Block Protect
-    BP1 = 0b00001000 # Block Protect
-    BP0 = 0b00000100 # Block Protect
+    BP3 = 0b00100000 # Block 3 Protect
+    BP2 = 0b00010000 # Block 2 Protect
+    BP1 = 0b00001000 # Block 1 Protect
+    BP0 = 0b00000100 # Block 0 Protect
     WEL = 0b00000010 # Write Enable Latch (0 = Write Disable, 1 = Write Enable)
     WIP = 0b00000001 # Write In Progress (0 = Not Writing, 1 = Writing)
 
 class CONFIG_REG0():
-    TB = 0b00001000 # Top/Bottom Protect Bit(Default = 0, 0=TOP 1=Bottom)
+    TB = 0b00001000 # Top/Bottom Protect Bit Addressing((Default = 0); 0 = Top To Bottom, 1 = Bottom To Top)
     
 class CONFIG_REG1():
-    LH = 0b00000010 # L/H Switch(0 = Ultra Low Power Mode(Default), 1 = High Performance Mode)
+    LH = 0b00000010 # L/H Switch((Default = 0); 0 = Ultra Low Power Mode, 1 = High Performance Mode)
     
